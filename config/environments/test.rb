b733a8332374595f2f9568c16a47d6be67863b5b
Dev Godzilla <dev.godzilla@gmail.com>
2015-05-19 22:34:00 +0900

    Finish user signup

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,6 @@ SampleApp2::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+  # bcryptのコスト関数を下げて、テスト速度を向上させる
+  ActiveModel::SecurePassword.min_cost = true
 end
